d49f31be39dff5a6004b7e055d7446b45760665b
Brian Leonard <brian.leonard@mongodb.com>
2020-11-20 11:50:53 -0500

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# atlas_search_apis
+# Atlas Search Index API Examples
+
+This respository contains examples of how to get, create and delete [Atlas Search](https://docs.atlas.mongodb.com/atlas-search/) indexes using the [API](https://docs.atlas.mongodb.com/api/). It uses [dotenv](https://www.npmjs.com/package/dotenv) to load the API credentials and  [digest_fetch](https://www.npmjs.com/package/digest-fetch) to make the API calls.
+
+## Usage
+
+Configure [Atlas Programmatic API Keys](https://docs.atlas.mongodb.com/configure-api-access/#programmatic-api-keys).
+
+`git clone wbleonard/atlas_search_index_apis`
+
+`npm install`
+
+`mv .env.example .env`
+
+Update `env` with your user and key values:
+
+```zsh
+  ATLAS_USER = "<ATLAS_PUBLIC_KEY>"
+  ATLAS_USER_KEY = "<ATLAS_PRIVATE_KEY>"
+```
+By default, the example code creates an index on the `sample_analytics.customer` collection:
+
+
+```JavaScript
+/* 
+ * Test Functions
+ */
+
+createSearchIndex("sample_analytics", "customers");
+
+getSearchIndex("sample_analytics", "customers").then((results) => {
+  console.log(JSON.stringify(results, null, " "));
+});
+
+//deleteExistingIndex();
+```
+## To Run
+```zsh
+✗ node app.js                                                             
+{
+ "collectionName": "customers",
+ "database": "sample_analytics",
+ "indexID": "5fb7f2320bca90461dc13b20",
+ "mappings": {
+  "dynamic": true
+ },
+ "name": "default"
+}
+[
+ {
+  "collectionName": "customers",
+  "database": "sample_analytics",
+  "indexID": "5fb7f2320bca90461dc13b20",
+  "mappings": {
+   "dynamic": true
+  },
+  "name": "default"
+ }
+]
+```
---
+++ app.js
@@ -0,0 +1,93 @@
+/*
+ *  Atlas Search API Examples
+ *  https://docs.atlas.mongodb.com/reference/api/atlas-search/
+ */
+require('dotenv').config();
+const DigestFetch = require("digest-fetch");
+
+
+const client = new DigestFetch(
+  process.env.ATLAS_USER,
+  process.env.ATLAS_USER_KEY,
+  {}
+);
+
+project_id = "5e8f8268d896f55ac04969a1";
+cluster_name = "SA-SHARED-DEMO";
+
+const urlbase = "https://cloud.mongodb.com/api/atlas/v1.0/";
+
+const body = {
+  collectionName: "customers",
+  database: "sample_analytics",
+  mappings: {
+    dynamic: true,
+  },
+  name: "default",
+};
+
+/* 
+ * Test Functions
+ */
+
+createSearchIndex("sample_analytics", "customers");
+
+getSearchIndex("sample_analytics", "customers").then((results) => {
+  console.log(JSON.stringify(results, null, " "));
+});
+
+//deleteExistingIndex();
+
+/*
+ * Impl functions
+ */
+
+async function getSearchIndex(database, collection) {
+  const response = await client.fetch(
+    urlbase +
+      `groups/${project_id}/clusters/${cluster_name}/fts/indexes/${database}/${collection}`,
+    {}
+  );
+  return response.json();
+}
+
+async function createSearchIndex(database, collection) {
+  client
+    .fetch(
+      urlbase + `groups/${project_id}/clusters/${cluster_name}/fts/indexes`,
+      {
+        method: "post",
+        body: JSON.stringify(body),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(JSON.stringify(json, null, " "));
+    });
+}
+
+async function deleteSearchIndex(index_id) {
+  client
+    .fetch(
+      urlbase +
+        `groups/${project_id}/clusters/${cluster_name}/fts/indexes/${index_id}`,
+      {
+        method: "delete",
+      }
+    )
+    .then((res) => res.json())
+    .then((json) => console.log(JSON.stringify(json, null, " ")));
+}
+
+async function deleteExistingIndex() {
+  getSearchIndex("sample_analytics", "customers").then((results) => {
+    console.log(JSON.stringify(results, null, " "));
+
+    if (results.length > 0) {
+      indexID = results[0].indexID;
+      console.log(indexID);
+      deleteSearchIndex(indexID);
+    }
+  });
+}
